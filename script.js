8b6c7954d1e8ccac2c148e79f73e2ad71517f0ca
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2020-12-05 13:40:18 +0800

    adding questions to script file

---
+++ script.js
@@ -0,0 +1,44 @@
+// set questions as object variables.
+const questions = [
+    {
+        question: "A variable that cannot be re-declared or assigned is defined as ...",
+        choices: ["const", "var", "let", "none of the above"],
+        answer: "const",
+        userAnswer: "const",
+        outcome: false,
+        time: 0
+    },
+    {
+        question: "Which are true of NaN?",
+        choices: ["It is a number", "It stand for Not a Number", "NaN != NaN", "All of the Above"],
+        answer: "All of the Above",
+        userAnswer: "All of the Above",
+        outcome: false,
+        time: 0
+    },
+   {
+        question: "Which is not a simple datatype and can not be mutated?",
+        choices: ["Numbers", "Strings", "Objects", "Boolean"],
+        answer: "Objects",
+        userAnswer: "Objects",
+        outcome: false,
+        time: 0
+    },
+   {
+        question: "Given y=5 in the following use of the % operater x = 10 % 3 then which is true for x",
+        choices: ["x = 0", "x = 1", "x = 2", "x = 3"],
+        answer: "x = 1",
+        userAnswer: "x = 1",
+        outcome: false,
+        time: 0
+    },
+  {
+        question: "How do you call a function named - myFunction?",
+        choices: ["call function myFunction()", "function myFunction()", "myFunction{}", "myFunction()"],
+        answer: "myFunction()",
+        userAnswer: "myFunction()",
+        outcome: false,
+        time: 0
+    }
+
+  ];
